c9a18e5568fa99a776dae50777ddc0ad0c4ab3c7
a4-01 <21400389@handong.edu>
2020-06-26 17:27:04 +0900

    폴더 정리

--- Korean/test_nicole/pop.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.trigger').click(function() {
-       $('#overlay').fadeIn(300);
-    });
- 
-    $('.close').click(function() {
-       $('#overlay').fadeOut(300);
-    });
- });
